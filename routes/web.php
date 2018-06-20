6ca0a2c54eb85ba4403312d2336b68bc126e964a
taitokita <taito.kita@rakuten.com>
2018-06-20 06:12:03 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ Route::get('/', function () {
 });
 
 Route::get('/', 'WelcomeController@index');
+
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
